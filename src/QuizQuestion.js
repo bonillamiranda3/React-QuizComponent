808cc5224bc2d2710094841d82ec11079325fd83
Miranda Bonilla <bonillamiranda3@gmail.com>
2018-10-14 21:28:53 -0400

    update

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import QuizQuestionButton,  from './QuizQuestionButton'
+import QuizQuestionButton, { Component } from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
   render() {
